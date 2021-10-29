6578f982dc19f46b66e3524a723908d147ea31d8
Anna Kostenko <anna.double.y@gmail.com>
2021-10-29 20:29:13 +0300

    created drawField

--- src/main/java/Battlefield.java
+++ src/main/java/Battlefield.java
@@ -1,2 +1,16 @@
 public class Battlefield {
+
+    private int battlefield1[][];
+    private int battlefield2[][];
+
+    public static void drawField(){
+
+        System.out.println(" 0 1 2 3 4 5 6 7 8 9");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + " - - - - - - - - -");
+
+        }
+    }
+
+
 }
