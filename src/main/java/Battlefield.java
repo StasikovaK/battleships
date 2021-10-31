d05c71042e61cf7154ff0048b267f2182ac18f7c
mgutane <maija.gutane@gmail.com>
2021-10-31 21:36:27 +0200

    battlefield2

--- src/main/java/Battlefield.java
+++ src/main/java/Battlefield.java
@@ -29,7 +29,7 @@ public class Battlefield {
         battlefield = new int[10][10];
         System.out.println(" 0 1 2 3 4 5 6 7 8 9");
         for (int i = 0; i < battlefield.length; i++) {
-            System.out.print(i + " -");
+            System.out.print(i );
             for (int j = 0; j < battlefield[1].length; j++) {
                 System.out.print(" -");
             }
